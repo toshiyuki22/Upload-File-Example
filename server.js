48562c3956f8ae7039d84be6c743e69cbe6859e7
miguel.tavares <miguel.tavares@pipecodes.com>
2020-05-19 18:42:07 +0100

    upload files server

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+app.use(fileUpload());
+
+
+// Uplaod Endpoint
+app.post('/upload', (req,res) => {
+    if(req.files === null) {
+        return res.status(400).json({msg: 'No file uploaded'})
+    }
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/${file.name}`, err => {
+        if(err)
+        {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
+    });
+
+});
+
+app.listen(5000, () => console.log('Server Started...'));
